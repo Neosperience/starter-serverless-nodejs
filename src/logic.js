b9221e4b225c6220e696bd55d0f4e67c9355f88d
Luca Bianchi <bianchi.luca@gmail.com>
2017-06-12 08:33:18 +0200

    Revertt logic to non ES6 classes. refs #7

--- src/logic.js
+++ src/logic.js
@@ -2,25 +2,17 @@
 
 const Promise = require('bluebird');
 
-let logger;
+const Logic = function (logger) {
 
-class Logic {
-
-    constructor (injectedLogger) { // logger param is passed into constructor and set to variable
-        logger = injectedLogger;
-    }
-
-    sayHello () { // example function required by logic
+    this.sayHello = function () { // example function required by logic
         logger.verbose('Logic/sayHello'); // injected object is called
         return Promise.try(function () { // always wrap into a Promise.try to handle errors
             return 'Hello World';
         });
-    }
+    };
 
-}
+};
 
 Logic.$inject = ['logger'];
 
-module.exports = function (logger) {
-    return new Logic(logger);
-};
+module.exports = Logic;
