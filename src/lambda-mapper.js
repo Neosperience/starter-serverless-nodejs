c81031a6a3c2d1bc70adc850390be837b436f653
Luca Bianchi <bianchi.luca@gmail.com>
2017-06-12 08:30:37 +0200

    Add lambda mapper layer, tests and schema for principal. refs #7

---
+++ src/lambda-mapper.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Promise = require('bluebird');
+const principalSchema = require('../resources/json-schemas/principal.json');
+
+const LambdaMapper = function (logger, logic, lambdaEvent) {
+
+    this.sayHello = function (event) {
+        logger.verbose('LambdaMapper/sayHelllo');
+        return Promise.try(function () {
+            var principal = lambdaEvent.extractPrincipalFromEvent(event, principalSchema);
+            return logic.sayHello(principal);
+        })
+            .then(function (result) {
+                
+                return lambdaEvent.buildSuccessResponseEvent(event, result);
+            })
+            .catch(function (error) {
+                return lambdaEvent.buildErrorResponseEvent(event, error, error.statusCode);
+            });
+    };
+
+};
+
+LambdaMapper.$inject = ['logger', 'logic', 'lambda-event'];
+
+module.exports = LambdaMapper;
